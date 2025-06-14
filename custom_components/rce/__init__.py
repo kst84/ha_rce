8e6c57c592dc8120b6adc9875cf1fe7fe7b9f316
kst84 <kstarzyk@gmail.com>
2025-06-14 17:52:12 +0200

    Update __init__.py

--- custom_components/rce/__init__.py
+++ custom_components/rce/__init__.py
@@ -15,20 +15,16 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up RCE integration."""
     _LOGGER.info("RCE-async_setup_entry " + str(entry))
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     _LOGGER.info("RCE-async_unload_entry remove entities")
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    )
-    return True
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    return unload_ok
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-     """Reload config entry."""
-     await async_unload_entry(hass, entry)
-     await async_setup_entry(hass, entry)
+    """Reload config entry."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
